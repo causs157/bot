745af8536fa1ca788a07325d9533a8f63c611746
causs157 <77118231+causs157@users.noreply.github.com>
2021-01-07 14:43:14 -0300

    Add files via upload

---
+++ Lib/help.js
@@ -0,0 +1,97 @@
+
+const help = (prefix) => { 
+	return `                 
+┏━━❉ *About Bot* ❉━━┓
+┣⊱ *${prefix}owner*
+┣⊱ *${prefix}donasi*
+┣⊱ *${prefix}info*
+┣⊱ *${prefix}botstat*
+┣⊱ *${prefix}bug*
+┣━━❀ *Maker* ❀━━
+┣⊱ *${prefix}ocr*
+┣⊱ *${prefix}sticker*
+┣⊱ *${prefix}tts*
+┣⊱ *${prefix}toimg*
+┣⊱ *${prefix}phlogo*
+┣⊱ *${prefix}quotemaker*
+┣⊱ *${prefix}ninjalogo*
+┣⊱ *${prefix}glitch*
+┣⊱ *${prefix}text3d*
+┣━━❀ *Kerang ajaib* ❀━
+┣⊱ *${prefix}kapankah*
+┣⊱ *${prefix}apakah*
+┣⊱ *${prefix}bisakah*
+┣⊱ *${prefix}rate*
+┣━━❀ *Fun* ❀━━━━
+┣⊱ *${prefix}meme*
+┣⊱ *${prefix}memeindo*
+┣⊱ *${prefix}hilih*
+┣⊱ *${prefix}fitnah*
+┣⊱ *${prefix}truth*
+┣⊱ *${prefix}dare*
+┣⊱ *${prefix}bucin*
+┣⊱ *${prefix}persengay*
+┣━━❀ *Anime* ❀━━
+┣⊱ *${prefix}loli*
+┣⊱ *${prefix}nsfwloli*
+┣⊱ *${prefix}anime*
+┣⊱ *${prefix}randomanime*
+┣⊱ *${prefix}randomhentai*
+┣⊱ *${prefix}nsfwloli*
+┣⊱ *${prefix}nsfwblowjob*
+┣⊱ *${prefix}nsfwneko*
+┣⊱ *${prefix}nsfwtrap*
+┣━━❀ *Media* ❀━━
+┣⊱ *${prefix}tiktokstalk*
+┣⊱ *${prefix}ytmp4*
+┣━━❀ *Other* ❀━━
+┣⊱ *${prefix}ping*
+┣⊱ *${prefix}shorturl*
+┣⊱ *${prefix}fototiktok*
+┣⊱ *${prefix}map*
+┣⊱ *${prefix}kbbi*
+┣⊱ *${prefix}artinama*
+┣⊱ *${prefix}infogempa*
+┣⊱ *${prefix}quotes*
+┣⊱ *${prefix}cerpen*
+┣⊱ *${prefix}lirik*
+┣⊱ *${prefix}pokemon*
+┣⊱ *${prefix}chord*
+┣⊱ *${prefix}blocklist*
+┣⊱ *${prefix}hidetag*
+┣⊱ *${prefix}wolflogo*
+┣⊱ *${prefix}lionlogo*
+┣⊱ *${prefix}ssweb*
+┣⊱ *${prefix}anjing*
+┣⊱ *${prefix}indohot*
+┣━━❀ *Group* ❀━━
+┣⊱ *${prefix}linkgc*
+┣⊱ *${prefix}tagall*
+┣⊱ *${prefix}add* [+62xx]
+┣⊱ *${prefix}group* [buka/tutup]
+┣⊱ *${prefix}demote* [tag]
+┣⊱ *${prefix}promote* [tag]
+┣⊱ *${prefix}kick*
+┣⊱ *${prefix}listadmin*
+┣⊱ *${prefix}simih* [1/0]
+┣⊱ *${prefix}nsfw* [1/0]
+┣⊱ *${prefix}welcome* [1/0]
+┣━━❀ *Owner bot* ❀━
+┣⊱ *${prefix}clearall*
+┣⊱ *${prefix}block*
+┣⊱ *${prefix}unblock*
+┣⊱ *${prefix}bc*
+┣⊱ *${prefix}leave*
+┣⊱ *${prefix}setpp*
+┣⊱ *${prefix}setprefix*
+┣━━━━━━━━━━━━━━
+┃    *BOT BY 𝙞𝙩𝙖𝙘𝙝𝙞𝙣⚡*
+┗━━━━━━━━━━━━━━
+
+`
+}
+exports.help = help
+
+
+
+  
